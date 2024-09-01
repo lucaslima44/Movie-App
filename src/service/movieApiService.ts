0476f0ad37484b7634f62fede0b05c6779c14405
lucaslima44 <lucaslima2011h@gmail.com>
2024-09-01 19:07:24 -0300

    fix: add header

--- src/service/movieApiService.ts
+++ src/service/movieApiService.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { tmdbApiKey } from "../keys/tmdb_api_key";
 
-const apiKey: string = tmdbApiKey;
-const baseUrl: string = "https://api.themoviedb.org/3";
+const apiKey: string = tmdbApiKey
+const baseUrl: string = 'https://api.themoviedb.org/3'
+
 export const getPopularMovies = async () => {
-  try {
-    const response = await axios.get(
-      `${baseUrl}/movie/popular?api_key=${apiKey}`
-    );
-    return response.data.results;
-  } catch (error) {
-    console.error("Error fetching popular movies:", error);
-    //return [];
-  }
+    try {
+        const response = await axios.get(
+            `${baseUrl}/movie/popular`, {
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${apiKey}`
+                }
+            });
+        return response.data.results;
+    } catch (error) {
+        console.error("Error fetching popular movies", error);
+        //return [];
+    }
 };
